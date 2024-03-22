41e95cf687fc23818ab4a7aa9c2fdc83a10b2b7f
AlejandroSuero <asuerome@gmail.com>
2024-03-22 13:26:52 +0100

    feat(main): implemented `handleMessage` with logger and msg

--- main.go
+++ main.go
@@ -15,11 +15,13 @@ func main() {
 	scanner.Split(rpc.Split)
 	for scanner.Scan() {
 		msg := scanner.Text()
-		handleMessage(msg)
+		handleMessage(logger, msg)
 	}
 }
 
-func handleMessage(_ any) {}
+func handleMessage(logger *log.Logger, msg any) {
+	logger.Println(msg)
+}
 
 func getLogger(filename string) *log.Logger {
 	pwd, err := os.Getwd()
