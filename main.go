b4f7752db67b725a0120c62b996968b8953b5f44
AlejandroSuero <asuerome@gmail.com>
2024-03-22 13:00:51 +0100

    feat(main): added logger

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bufio"
+	"log"
 	"lsp/rpc"
 	"os"
 )
 
 func main() {
+	logger := getLogger("/logs/log.txt")
+	logger.Println("Logger started")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(rpc.Split)
 	for scanner.Scan() {
@@ -16,3 +19,15 @@ func main() {
 }
 
 func handleMessage(_ any) {}
+
+func getLogger(filename string) *log.Logger {
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	logfile, err := os.OpenFile(pwd+filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("hey, this is not a good file my guy")
+	}
+	return log.New(logfile, "[lsp-practice]", log.Ldate|log.Ltime|log.Lshortfile)
+}
